bef8d2e84c403984543d95e69cc9d86d0ff8aa0f
sibi12325 <sibimoothedan@hotmail.com>
2024-09-17 21:58:51 +0800

    input

--- app/graph.py
+++ app/graph.py
@@ -6,17 +6,20 @@ g = Graph()
 
 bob = URIRef("http://example.org/people/Bob")
 linda = BNode() # a GUID is generated
+
 name = Literal("Bob") # passing a string
 age = Literal(24) # passing a python int
 height = Literal(76.5) # passing a python float
+
 g.add((bob, RDF.type, FOAF.Person))
 g.add((bob, FOAF.name, name))
 g.add((bob, FOAF.age, age))
+
 g.add((bob, FOAF.knows, linda))
+g.add((linda, FOAF.knows, bob))
+
 g.add((linda, RDF.type, FOAF.Person))
 g.add((linda, FOAF.name, Literal("Linda")))
 
-print(g.serialize())
-
-text = g.serialize()
+graph = g.serialize()
 
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,12 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
-from .graph import text
+from .graph import graph
 
 @app.route('/')
 def index():
-	return render_template('index.html', graph=text)
+	return render_template('index.html', graph=graph)
+
+@app.route('/query', methods=['POST'])
+def query():
+	query_input = request.form.get('query')  # Retrieves the input with the 'name' attribute 'query'
+	return render_template('index.html', graph=query_input)
