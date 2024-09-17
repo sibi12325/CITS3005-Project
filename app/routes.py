795d1981bf1ead691a3a4c37adf89ccecbaa42f3
sibi12325 <sibimoothedan@hotmail.com>
2024-09-17 22:15:28 +0800

    made less ugly

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,15 @@
 from flask import render_template, request
 from app import app
-from .graph import graph
+from .graph import graph, g
 
 @app.route('/')
 def index():
-	return render_template('index.html', graph=graph)
+	result = "Result will be displayed here"
+	return render_template('index.html', graph=graph, result=result)
 
 @app.route('/query', methods=['POST'])
 def query():
-	query_input = request.form.get('query')  # Retrieves the input with the 'name' attribute 'query'
-	return render_template('index.html', graph=query_input)
+	query_input = request.form.get('query')
+	result = g.query(f"""{query_input}""")
+	result = result.serialize()
+	return render_template('index.html', graph=query_input, result=result)
