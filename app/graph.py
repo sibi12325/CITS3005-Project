bd55be4d0423fbb02eeb1d1de539cdd98409f4e6
sibi12325 <sibimoothedan@hotmail.com>
2024-09-17 21:36:12 +0800

    random graph

---
+++ app/graph.py
@@ -0,0 +1,22 @@
+from rdflib import Graph
+from rdflib import URIRef, BNode, Literal
+from rdflib.namespace import FOAF, RDF
+
+g = Graph()
+
+bob = URIRef("http://example.org/people/Bob")
+linda = BNode() # a GUID is generated
+name = Literal("Bob") # passing a string
+age = Literal(24) # passing a python int
+height = Literal(76.5) # passing a python float
+g.add((bob, RDF.type, FOAF.Person))
+g.add((bob, FOAF.name, name))
+g.add((bob, FOAF.age, age))
+g.add((bob, FOAF.knows, linda))
+g.add((linda, RDF.type, FOAF.Person))
+g.add((linda, FOAF.name, Literal("Linda")))
+
+print(g.serialize())
+
+text = g.serialize()
+
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app import app
+from .graph import text
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('index.html', graph=text)
